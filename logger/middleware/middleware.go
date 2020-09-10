4028bc3dff10b60ee1a61fc98283967bbc446345
Nikolay Yakubitskiy <steseagal87@gmail.com>
2020-09-10 14:32:47 +0300

    update middleware to work with zap logger

--- logger/middleware/middleware.go
+++ logger/middleware/middleware.go
@@ -3,12 +3,23 @@ package middleware
 import (
 	"log"
 	"net/http"
+
+	"github.com/reddaemon/apiproject/config"
+	lh "github.com/reddaemon/apiproject/handlers"
+	"github.com/reddaemon/apiproject/logger"
 )
 
 //LogRequestHandler logging all requests
 func LogRequestHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		c, _ := config.GetConfig(*lh.ConfigPath)
+		l, err := logger.GetLogger(c)
+		if err != nil {
+			log.Fatalf("Cannot get logger %v", err)
+		}
+		lsugar := l.Sugar()
+		//log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		lsugar.Infof("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 		h.ServeHTTP(w, r)
 	})
 }
