519ca628184ef023a4886fba95a1d9830f476dbf
Nikolay Yakubitskiy <steseagal87@gmail.com>
2020-09-10 14:43:07 +0300

    update logger with fix deffer fixes

--- logger/logger.go
+++ logger/logger.go
@@ -17,15 +17,29 @@ func GetLogger(cfg *config.Config) (*zap.Logger, error) {
 	switch cfg.Environment {
 	case "production":
 		l, err = zap.NewProduction()
+		defer func() {
+
+			err := l.Sync()
+			if err != nil {
+				l.Fatal(err.Error())
+			}
+		}()
 		if err != nil {
 			return nil, err
 		}
 	case "dev":
 		l, err = zap.NewDevelopment()
-		defer l.Sync()
+		defer func() {
+
+			err := l.Sync()
+			if err != nil {
+				l.Fatal(err.Error())
+			}
+		}()
 		if err != nil {
 			return nil, err
 		}
+
 	default:
 		l = zap.NewExample()
 	}
